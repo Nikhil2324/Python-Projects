c8686dbb45bfc535b6418a470517822245c4d91f
Nikhil2324 <nikhil2324@g.ucla.edu>
2014-11-11 21:42:06 -0800

    Updated LakerBot.py - Removed login info
    Searches /r/nba for current popular threads about Kobe or the Lakers and alerts me through my Reddit inbox

--- LakerAlertBot.py
+++ LakerAlertBot.py
@@ -1,7 +1,7 @@
 import praw, time
 
-r = praw.Reddit('Learning Python - PRAW related question monitor by BeezInTheTrap')
-r.login('BeezInTheTrap', 'air23jordan')
+r = praw.Reddit('Learning Python - PRAW related question monitor by Nikhil Gupta')
+r.login('username', 'password') #replace username and password with yours
 already_done = []
 count = 1;
 
@@ -15,8 +15,8 @@ while True:
         has_praw = (any(string in op_text for string in prawWords) or any(string in op_title for string in prawWords))
         if submission.id not in already_done and has_praw:
             msg = '[Laker Thread](%s)' % submission.short_link
-            r.send_message('BeezInTheTrap', 'Laker Fan Alert', msg)
+            r.send_message('username', 'Laker Fan Alert', msg) #replace username with your username
             already_done.append(submission.id)
-    print "Done Execution #%i" % count
-    count += 1
+    #print "Done Execution #%i" % count
+    #count += 1
     time.sleep(1800)
