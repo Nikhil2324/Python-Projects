836549a159c67ff6fdbc3803936dd4c4ee101d9c
Nikhil2324 <nikhil2324@g.ucla.edu>
2015-01-22 07:07:59 -0800

    Update lakerStreams.py

--- lakerStreams.py
+++ lakerStreams.py
@@ -18,33 +18,24 @@ r.login('username', 'password')
 title_flags = ['game', 'laker']
 seen_comments = set()
 streams = list()
-number = 1 #debugging
-
-#print("Done setup") #debugging
 
 while True:
     subreddit = r.get_subreddit('nba')
-    for submission in subreddit.get_hot(limit=50):
+    for submission in subreddit.get_hot(limit=50): #get 50 most popular posts at the moment (game threads are usually popular, if not in the top 50, they haven't been created yet)
         title = submission.title.lower()
-        if all(submission in title for submission in title_flags):
-            #print("Found submission") #debugging
-            submission.replace_more_comments(limit=None, threshold=0)
-            #print("Replaced comments") #debugging
+        if all(submission in title for submission in title_flags): #if the submission title has 'laker' and 'game' in it, it is the game thread
+            submission.replace_more_comments(limit=None, threshold=0) #get ALL the comments (just in case some links are buried)
             forest_comments = submission.comments
-            #print("Forest comment format") #debugging
-            #forest_comments.sort(key=lambda comment: comment.score) #Sorts by popularity, but not worth it for performance reasons
-            #print "Sorted comments" #debugging
+            #forest_comments.sort(key=lambda comment: comment.score) #Sorts by popularity, but not worth it for performance reasons in my opnion
             for comment in forest_comments:
                 if comment.id not in seen_comments and ("stream" in comment.body.lower() or "link" in comment.body.lower()):
-                    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', comment.body)
+                    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', comment.body) #regex magic to find links in the html source
                     for x in urls:
-                        streams.append(x)
+                        streams.append(x) #each x is a link, put it in the list
                 seen_comments.add(comment.id)
             break #should only be 1 game thread, so need to continue looking
 
-    #print("Searched through submissions") #debugging
-
-    content = '\n'.join(streams)
+    content = '\n'.join(streams) #join each link with a newline
     msg = MIMEText(content, TEXT_SUBTYPE)
     msg["Subject"] = SUBJECT
     msg["From"] = SENDER
@@ -55,10 +46,8 @@ while True:
         smtpObj.login(user=SENDER, password="password")
         smtpObj.sendmail(SENDER, RECEIVER, msg.as_string())
         smtpObj.quit();
-        #print ("Sent email")
+        print ("Sent email")
     except SMTPException as error:
         print("ERROR: Unable to send mail")   
     
-    print "Done Execution Number %i" % number
-    number += 1
-    time.sleep(900)
+    time.sleep(900) #wait 15 minutes, see if new links are there
